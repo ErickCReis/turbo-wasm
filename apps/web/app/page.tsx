caac5b06769b89483b087cc67ba022191654485c
ErickCReis <erickcorreareis@gmail.com>
2024-07-14 22:38:31 -0300

    fix: typescript

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
-import { get_factorial, initExampleRust } from "@repo/rust-lib/lib";
+import { get_factorial, initExampleRust } from "@repo/rust-lib";
 
 export default function Home() {
   const [value, setValue] = useState(0);

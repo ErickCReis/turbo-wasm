8c4156415785e744a446d00efbcea66948ca70a4
ErickCReis <erickcorreareis@gmail.com>
2024-07-14 22:19:51 -0300

    input

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,25 +1,34 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
 import { get_factorial, initExampleRust } from "@repo/rust-lib/lib";
 
 export default function Home() {
+  const [value, setValue] = useState(0);
+  const [result, setResult] = useState("");
+
   useEffect(() => {
     initExampleRust();
   }, []);
 
   return (
     <div className={styles.page}>
+      <input
+        type="number"
+        value={value}
+        onChange={(e) => setValue(parseInt(e.target.value))}
+      />
       <button
         onClick={async () => {
-          const factorial = await get_factorial(11);
-          alert(factorial);
+          const factorial = await get_factorial(value);
+          setResult(factorial);
         }}
       >
         Factorio
       </button>
+      <div>Result: {result}</div>
     </div>
   );
 }

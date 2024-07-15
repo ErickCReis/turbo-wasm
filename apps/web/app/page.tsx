ae716d00dc3fcbe524b00262778be33aafa6cec0
ErickCReis <erickcorreareis@gmail.com>
2024-07-14 22:15:24 -0300

    script and init

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,20 @@
 "use client";
 
+import { useEffect } from "react";
 import styles from "./page.module.css";
 
 import { get_factorial, initExampleRust } from "@repo/rust-lib/lib";
 
 export default function Home() {
+  useEffect(() => {
+    initExampleRust();
+  }, []);
+
   return (
     <div className={styles.page}>
       <button
         onClick={async () => {
-          await initExampleRust();
-          const factorial = await get_factorial(10);
+          const factorial = await get_factorial(11);
           alert(factorial);
         }}
       >
